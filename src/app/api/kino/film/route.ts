6faec964d06987deebd4c2adb1e4cc9b6e57b840
almaz <almaz@local>
2026-04-19 00:53:27 +0500

    feat: update kinopoisk api key to restore service

--- src/app/api/kino/film/route.ts
+++ src/app/api/kino/film/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-const API_KEY = '5ee2ab49-8a04-436d-ae88-cf6943b51018'
+const API_KEY = '9376200e-5090-448f-93f2-4e8b4ccbde6b'
 const BASE = 'https://kinopoiskapiunofficial.tech/api'
 
 export async function GET(req: NextRequest) {
